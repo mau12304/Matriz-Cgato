cd33e4fd272f2a2be239754894cff5219abd8b55
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-03 13:00:36 -0600

    Agregar archivos de proyecto.

---
+++ Matriz c#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class program
+{
+    const int maxf = 3;
+    const int maxc = 5;
+
+    static void Main(string[] args)
+    {
+        float[,] a = new float[maxf, maxc];
+        int f, c;
+        //leer el array
+        for (f = 0; f < maxf; f++)
+        {
+            for (c = 0; c < maxc; c++)
+            {
+                a[f, c] = float.Parse(Console.ReadLine());
+            }
+        }
+        //Escribir el array
+        for (f = 0; f < maxf; f++)
+        {
+            for (c = 0; c < maxc; c++)
+            {
+                Console.Write(a[f, c] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
